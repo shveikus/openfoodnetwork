8766a7d514eee736f69827606fc80af286d9c813
shveikus <dmitry.shveikus@ecomcharge.com>
2021-06-15 10:12:19 +0300

    Update default redis port for Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_jobs_url = ENV.fetch("OFN_REDIS_JOBS_URL", "redis://localhost:6381/0")
+redis_jobs_url = ENV.fetch("OFN_REDIS_JOBS_URL", "redis://localhost:6379/0")
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_jobs_url, network_timeout: 5 }
